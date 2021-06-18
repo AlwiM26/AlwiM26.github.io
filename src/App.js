1a6fce794a6e818b5ea5e51569c995a62eff991b
AlwiM26 <alwimhd01@gmail.com>
2021-06-19 00:11:19 +0700

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import ProjectDetail from "./components/ProjectDetail";
 import Navbar from "./components/Navbar/Navbar";
